55f5e620c8bd529a2c5fa9f0113b551f0b5fc5c0
Adaia León <adaialeoncts@gmail.com>
2022-12-02 12:40:53 +0100

    4

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -5,119 +5,57 @@ using UnityEngine.SceneManagement;
 
 public class Raycast : MonoBehaviour
 {
+    [SerializeField]private LayerMask rayLayer;
+    private GameObject hitName;
 
-   [SerializeField]private LayerMask ignoreLayer;
 
-
-    void update()
+    void Update()
     {
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit, 10f, ignoreLayer))
-        {
-            //DEVOLVER POSICION
-            Vector3 hitPosition = hit.point;
-		    //DEVOLVER NOMBRE
-            string hitName = hit.transform.name;
-		    //DEVOLVER TAG
-            string hitTag = hit.transform.tag;
-		    //DEVOLVER DISTANCIA
-            float hitDistance = hit.distance;
-		    //ANIMATOR
-            Animator hitAnim = hit.transform.gameObject.GetComponent<Animator>();
-		    //DIBUJAR EL RAYO DE COLOR
-            Debug.DrawRay(transform.position, transform.forward * hitDistance, Color.green);
-        }
-
-        //Detecta la esfera
-        if(hitTag == "Esfera")
-        {
-
-            Debug.Log("impacto en obstaculo");
-
-        }
-        else 
-        {
-            Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
-        }
-        if(Input.GetButtonDown("Fire1"))
+          
+    //Si le damos al botón de disparar(botón izquierdo del ratón), que nos muestre los nombres de los objetos que clickamos 
+    if(Input.GetButtonDown("Fire1"))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit rayHit;
 
-            if(Physics.Raycast(ray, out rayHit))
+            if(Physics.Raycast(ray, out rayHit, rayLayer))
             {
-                Debug.Log(rayHit.transform.name);
+
+                Debug.Log(rayHit.point);
+                transform.position = new Vector3(rayHit.point.x,transform.position.y ,rayHit.point.z);
+                
+
+                hitName = rayHit.transform.gameObject;
+                Debug.Log(hitName);
+
+                LoadLevel();
+
+                
             }
-        }
-
-        //Detecta el cubo grande
-        if(hitTag == "CuboGrande")
-        {
-
-            Debug.Log("impacto en obstaculo");
-
-        }
-        else 
-        {
-            Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
-        }
-        if(Input.GetButtonDown("Fire1"))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit rayHit;
-
-            if(Physics.Raycast(ray, out rayHit))
-            {
-                Debug.Log(rayHit.transform.name);
-            }
-        }
-
-        //Detecta el cubo pequeño
-        if(hitTag == "CuboPequeño")
-        {
-
-            Debug.Log("impacto en obstaculo");
-
-        }
-        else 
-        {
-            Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
-        }
-        if(Input.GetButtonDown("Fire1"))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit rayHit;
-
-            if(Physics.Raycast(ray, out rayHit))
-            {
-                Debug.Log(rayHit.transform.name);
-            }
-        }
-
-        /*if(("Fire1" == "CuboGrande"))
-        {
-            SceneManager.LoadScene("Scene1 1");
-
-        } 
-
-        if((Fire1 == "Esfera"))
-        {
-            SceneManager.LoadScene("Scene1 2");
-
-        }  
-
-        if((Fire1 == "CuboPequeño"))
-        {
-            SceneManager.LoadScene("Scene1 3");
-
-        }*/
-    
+        }        
     }
 
-    void start()
+    void LoadLevel()
     {
 
-    }
-   
+        if(hitName.gameObject.layer == 6)
+        {
 
+            SceneManager.LoadScene(1);
+
+        }
+        if(hitName.gameObject.layer == 7)
+        {
+
+            SceneManager.LoadScene(2);
+
+        }
+        if(hitName.gameObject.layer == 8)
+        {
+
+            SceneManager.LoadScene(3);
+
+        }
+
+    }
 }
